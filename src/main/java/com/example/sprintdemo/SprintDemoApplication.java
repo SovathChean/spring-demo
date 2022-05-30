c96b8cb8574ab38b7a780ef85c29a1229f6a0788
SOVATH CHEAN <sovath@wiresk.com>
2022-05-30 16:06:45 +0700

    init

---
+++ src/main/java/com/example/sprintdemo/SprintDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintdemo/SprintDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
